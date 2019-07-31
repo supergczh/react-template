7f321500781571ca87a4977df4264c399f637070
一秋 <37320849+supergczh@users.noreply.github.com>
2019-07-31 22:10:55 +0800

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,7 +5,7 @@ const context = require.context('./', false, /\.js$/);
 const reducerModules={};
 
 context.keys().filter(item => item !== './index.js').forEach(key => {
-  reducerModules[context(key).default.name]=context(key).default}
+  reducerModules[key.slice(2,-3)]=context(key).default}
 )
 
 const rootReducer = combineReducers(reducerModules)
