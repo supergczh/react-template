771884e2618488fed474ea7c3c6cadb6b5aa1704
一秋 <37320849+supergczh@users.noreply.github.com>
2019-08-10 16:57:33 +0800

    Update webpack.dll.js

--- webpack/webpack.dll.js
+++ webpack/webpack.dll.js
@@ -10,17 +10,15 @@ module.exports = {
     library: Object.keys(dependencies),
   },
   output: {
-    path: path.join(__dirname, '../dll'), // 生成的dll.js路径，我是存在/build/dev中
-    filename: '[name]_[chunkhash:8].dll.js', // 生成的文件名字
-    library: '[name]_[chunkhash:8]', // 生成文件的一些映射关系，与下面DllPlugin中配置对应
+    path: path.join(__dirname, '../dll'), 
+    filename: '[name]_[chunkhash:8].dll.js', 
+    library: '[name]_[chunkhash:8]', 
   },
   plugins: [
     new CleanWebpackPlugin(),
-    // 使用DllPlugin插件编译上面配置的NPM包
     new webpack.DllPlugin({
-      // 会生成一个json文件，里面是关于dll.js的一些配置信息
       path: path.join(__dirname, '../dll', '[name].manifest.json'),
-      name: '[name]_[chunkhash:8]', // 与上面output中配置对应
+      name: '[name]_[chunkhash:8]', 
       context: __dirname,
     }),
   ],
